9bc455746acf81731ab1e65a58d2029738ad0766
Erik Henrique <erik.hac@outlook.com>
2019-04-20 08:24:20 -0300

    Second class - Column and Texts

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
       title: "People Counter",
-      home: Container(
-        color: Colors.white,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Pessoas: 0",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          Text("Pode Entrar!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0))
+        ],
       )));
 }
